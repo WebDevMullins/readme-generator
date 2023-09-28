e59745732b1dd9812fd4ef87c82319374997ee42
WebDevMullins <webdevmullins@gmail.com>
2023-09-28 14:00:47 -0500

    update license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,15 +16,33 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+	if (license !== 'None') {
+		return `- [License](#license)`
+	} else {
+		return ''
+	}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+	if (license !== 'None') {
+		return `## License
+		
+This project is licensed under the ${license} license.
+		
+${renderLicenseBadge(license)}`
+	} else {
+		return ''
+	}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 	const licenseBadge = renderLicenseBadge(data.license)
+	const licenseLink = renderLicenseLink(data.license)
+	const licenseSection = renderLicenseSection(data.license)
 	return `
 # ${data.title}
 
@@ -34,23 +52,28 @@ ${licenseBadge}
 - [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
-- [Liscense](#liscense)
+${licenseLink}
 - [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
 
 ## Description
 ${data.description}
+
 ## Installation
 ${data.installation}
+
 ## Usage
 ${data.usage}
-## Liscense
-${data.license}
+
+${licenseSection}
+
 ## Contributing
 ${data.contributing}
+
 ## Tests
-${data.test}
+${data.tests}
+
 ## Questions
 For any questions, please reach out at [GitHub](https://github.com/${data.githubUsername}) or email at ${data.email}.
 `
