482f71309e8f0f219f7c754c22c456874d29ce6a
WebDevMullins <webdevmullins@gmail.com>
2023-09-28 11:00:01 -0500

    add question and prompt

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = []
+const questions = [
+	{
+		type: 'input',
+		name: 'title',
+		message: 'Enter the project title:'
+	}
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+	inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init()
