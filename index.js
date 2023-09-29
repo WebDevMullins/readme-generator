8da4aa5399f5e595ace4dd51688bfd41ce911360
WebDevMullins <webdevmullins@gmail.com>
2023-09-29 10:19:00 -0500

    created README writes to output folder

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function writeToFile(fileName, data) {
 // Start prompt of questions to user and write to file
 function init() {
 	inquirer.prompt(questions).then((response) => {
-		writeToFile('README.md', generateMarkdown(response))
+		writeToFile('./output/README.md', generateMarkdown(response))
 	})
 }
 
