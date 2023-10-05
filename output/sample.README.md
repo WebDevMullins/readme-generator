e2579451320f26fcfc823b6151a44cf1ffa8a8a0
WebDevMullins <webdevmullins@gmail.com>
2023-10-05 13:50:52 -0500

    update MIT color and License section

---
+++ output/sample.README.md
@@ -0,0 +1,45 @@
+# Sample Title
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+Sample Description
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+Sample Installation Instructions
+
+## Usage
+
+Sample Usage Information
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+
+Sample Contribution Guidelines
+
+## Tests
+
+Sample Test Instructions
+
+## Questions
+
+For any additional questions, please reach out -
+
+GitHub: [WebDevMullins](https://github.com/WebDevMullins)
+
+Email: brandon@webdevmullins.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,9 +2,9 @@
 function renderLicenseBadge(license) {
 	switch (license) {
 		case 'MIT':
-			return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+			return '[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)'
 		case 'Apache 2.0':
-			return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+			return '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
 		case 'GPL 3.0':
 			return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
 		case 'BSD 3-Clause':
@@ -24,14 +24,22 @@ function renderLicenseSection(license) {
 	return license !== 'None'
 		? `## License
 		
-This project is licensed under the ${license} license.
-		
-${renderLicenseBadge(license)}`
+This project is licensed under the ${license} license.`
 		: ''
 }
 
 //  Display all response
-function generateMarkdown( { title, description, installation, usage, contributing, tests, license, githubUsername, email }) {
+function generateMarkdown({
+	title,
+	description,
+	installation,
+	usage,
+	contributing,
+	tests,
+	license,
+	githubUsername,
+	email
+}) {
 	const licenseBadge = renderLicenseBadge(license)
 	const licenseLink = renderLicenseLink(license)
 	const licenseSection = renderLicenseSection(license)
